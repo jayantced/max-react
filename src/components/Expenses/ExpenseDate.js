a29eff90c130978ed8574c6d8f7b002b0c8f5ef8
jayantced739 <109576199+jayantced@users.noreply.github.com>
2023-09-30 16:59:46 +0530

    added expenses folder

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,7 +1,7 @@
 import './ExpenseDate.css'
 
 function ExpenseDate({date}) {
-
+  
     const month = date.toLocaleString('en-US', {month: 'long'});
     const day = date.toLocaleString('en-US', {day: '2-digit'});
     const year = date.getFullYear();
